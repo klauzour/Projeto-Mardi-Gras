db53ea97aef35dd247ab8fcdc3dd3837d45118cd
GabrielHCassiano <gabriel.h.cassiano@gmail.com>
2024-01-28 19:39:00 -0300

    menu?

--- Projeto Mardi Gras/Assets/Scripts/Menu/MenuManager.cs
+++ Projeto Mardi Gras/Assets/Scripts/Menu/MenuManager.cs
@@ -28,7 +28,7 @@ public class MenuManager : MonoBehaviour
     public void Play()
     {
         // Trocar "play" pela cena certa 
-        SceneManager.LoadScene("Play");
+        SceneManager.LoadScene(1);
     }
 
     public void Config()
